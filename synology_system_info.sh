65e0672cc12336fd6396d10a248f692478549c16
Ricky Pai <rickyp999+github@gmail.com>
2020-11-26 07:07:48 -0800

    prettier formatting

--- synology_system_info.sh
+++ synology_system_info.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
-echo "Synology System Info:"
+echo "=== Synology System Info Start ==="
+echo
 echo $(cat /etc/synoinfo.conf | grep upnpmodelname)
 echo $(cat /etc.defaults/VERSION | grep buildnumber)
 echo $(cat /etc.defaults/VERSION | grep productversion)
 echo
-echo "Memory Info:"
+echo "=== Synology System Info End ==="
+echo
+echo "=== Memory Info Start ==="
+echo
 echo $(sudo dmidecode --type memory | grep "Manufacturer" | sed -e 's/^[[:space:]]*//')
 echo $(sudo dmidecode --type memory | grep "Part Number" | sed -e 's/^[[:space:]]*//')
 echo $(sudo dmidecode --type memory | grep "Size" | sed -e 's/^[[:space:]]*//')
 echo
-echo "HDD Info:"
+echo "=== Memory Info End ==="
+echo
+echo "=== HDD Info Start ==="
 for f in /dev/sata?; do
+	echo
   echo $(sudo smartctl -a $f | grep "Vendor")
 	echo $(sudo smartctl -a $f | grep "Product")
 	echo $(sudo smartctl -a $f | grep "User Capacity")
 	echo $(sudo smartctl -a $f | grep "Rotation Rate")
 done
+echo
+echo "=== HDD Info End ==="
