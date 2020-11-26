4a3e557395926ecc0bf3831e14f0966e4a5f5337
Ricky Pai <rickyp999+github@gmail.com>
2020-11-26 07:20:20 -0800

    guess i dont need echo here

--- synology_system_info.sh
+++ synology_system_info.sh
@@ -2,9 +2,9 @@
 
 echo "=== Synology System Info Start ==="
 echo
-echo $(cat /etc/synoinfo.conf | grep upnpmodelname)
-echo $(cat /etc.defaults/VERSION | grep buildnumber)
-echo $(cat /etc.defaults/VERSION | grep productversion)
+cat /etc/synoinfo.conf | grep upnpmodelname
+cat /etc.defaults/VERSION | grep buildnumber
+cat /etc.defaults/VERSION | grep productversion
 echo
 echo "=== Synology System Info End ==="
 echo
@@ -12,9 +12,9 @@ echo "=== Memory Info Start ==="
 echo
 mem_info_tmp_file=$(mktemp)
 sudo dmidecode --type memory > ${mem_info_tmp_file}
-echo $(cat ${mem_info_tmp_file} | grep "Manufacturer" | sed -e 's/^[[:space:]]*//')
-echo $(cat ${mem_info_tmp_file} | grep "Part Number" | sed -e 's/^[[:space:]]*//')
-echo $(cat ${mem_info_tmp_file} | grep "Size" | sed -e 's/^[[:space:]]*//')
+cat ${mem_info_tmp_file} | grep "Manufacturer" | sed -e 's/^[[:space:]]*//'
+cat ${mem_info_tmp_file} | grep "Part Number" | sed -e 's/^[[:space:]]*//'
+cat ${mem_info_tmp_file} | grep "Size" | sed -e 's/^[[:space:]]*//'
 echo
 echo "=== Memory Info End ==="
 echo
@@ -23,10 +23,10 @@ for f in /dev/sata?; do
 	echo
 	hdd_info_tmp_file=$(mktemp)
 	sudo smartctl -a ${f} > ${hdd_info_tmp_file}
-	echo $(cat ${hdd_info_tmp_file} | grep "Vendor")
-	echo $(cat ${hdd_info_tmp_file} | grep "Product")
-	echo $(cat ${hdd_info_tmp_file} | grep "User Capacity")
-	echo $(cat ${hdd_info_tmp_file} | grep "Rotation Rate")
+	cat ${hdd_info_tmp_file} | grep "Vendor"
+	cat ${hdd_info_tmp_file} | grep "Product"
+	cat ${hdd_info_tmp_file} | grep "User Capacity"
+	cat ${hdd_info_tmp_file} | grep "Rotation Rate"
 done
 echo
 echo "=== HDD Info End ==="
