a63fb298178daaa3f68c60d0c8790a28cf56af3b
Ricky Pai <rickyp999+github@gmail.com>
2020-11-26 11:45:26 -0800

    spread write tests out instead of repeating same block size

--- synology_disk_benchmark.sh
+++ synology_disk_benchmark.sh
@@ -31,6 +31,10 @@ echo
 # record the commands run
 set -o xtrace
 
+function cleanup {
+	rm -f test
+}
+
 function clear_cache {
 	sync
 	echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
@@ -39,8 +43,6 @@ function clear_cache {
 # runs a command 5 times with cache cleared
 # example: `5times echo hi`
 function 5times {
-	clear_cache
-
 	for i in {1..5}; do
 		$*
 	done
@@ -48,70 +50,74 @@ function 5times {
 
 function dd_read_tests {
 	# read from test file, read 1 GiB each time, read 2 times, total read 2 GiB
-	5times dd if=test of=/dev/null bs=1G count=2
+	clear_cache && dd if=test of=/dev/null bs=1G count=2
 
 	echo
 
 	# read from test file, read 128 MiB each time, read 8 times, total read 1 GiB
-	5times dd if=test of=/dev/null bs=128M count=8
+	clear_cache && dd if=test of=/dev/null bs=128M count=8
 
 	echo
 
 	# read from test file, read 1 MiB each time, read 1024 times, total read 1 GiB
-	5times dd if=test of=/dev/null bs=1M count=1024
+	clear_cache && dd if=test of=/dev/null bs=1M count=1024
 
 	echo
 
 	# read from test file, read 128 KiB each time, read 1024 times, total read 128 MiB
-	5times dd if=test of=/dev/null bs=128k count=1024
+	clear_cache && dd if=test of=/dev/null bs=128k count=1024
 
 	echo
 
 	# read from test file, read 4 KiB each time, read 1024 times, total read 4 MiB
-	5times dd if=test of=/dev/null bs=4k count=1024
+	clear_cache && dd if=test of=/dev/null bs=4k count=1024
 
 	echo
 
 	# read from test file, read 512 bytes each time, read 1024 times, total read 512 KiB
-	5times dd if=test of=/dev/null bs=512 count=1024
+	clear_cache && dd if=test of=/dev/null bs=512 count=1024
 }
 
 function dd_write_tests {
 	# write 0 to test file, write 1 GiB each time, write 2 times, total write 2 GiB
-	5times dd if=/dev/zero of=test bs=1G count=2 oflag=dsync
+	clear_cache && dd if=/dev/zero of=test bs=1G count=2 oflag=dsync
 
 	echo
 
 	# write 0 to test file, write 128 MiB each time, write 8 times, total write 1 GiB
-	5times dd if=/dev/zero of=test bs=128M count=8 oflag=dsync
+	clear_cache && dd if=/dev/zero of=test bs=128M count=8 oflag=dsync
 
 	echo
 
 	# write 0 to test file, write 1 MiB each time, write 1024 times, total write 1 GiB
-	5times dd if=/dev/zero of=test bs=1M count=1024 oflag=dsync
+	clear_cache && dd if=/dev/zero of=test bs=1M count=1024 oflag=dsync
 
 	echo
 
 	# write 0 to test file, write 128 KiB each time, write 1024 times, total write 128 MiB
-	5times dd if=/dev/zero of=test bs=128k count=1024 oflag=dsync
+	clear_cache && dd if=/dev/zero of=test bs=128k count=1024 oflag=dsync
 
 	echo
 
 	# write 0 to test file, write 4 KiB each time, write 1024 times, total write 4 MiB
-	5times dd if=/dev/zero of=test bs=4k count=1024 oflag=dsync
+	clear_cache && dd if=/dev/zero of=test bs=4k count=1024 oflag=dsync
 
 	echo
 
 	# write 0 to test file, write 512 bytes each time, write 1024 times, total write 512 KiB
-	5times dd if=/dev/zero of=test bs=512 count=1024 oflag=dsync
+	clear_cache && dd if=/dev/zero of=test bs=512 count=1024 oflag=dsync
 }
 
 function hdparm_read_timings {
+	clear_cache
+
 	# test read/cached read timings
-	5times sudo hdparm -Tt $device
+	sudo hdparm -Tt $device
 }
 
-hdparm_read_timings
+cleanup
+
+5times hdparm_read_timings
 
 echo
 
@@ -120,11 +126,11 @@ time openssl rand -out test $(echo 1G | numfmt --from=iec)
 
 echo
 
-dd_read_tests
+5times dd_read_tests
 
 echo
 
-dd_write_tests
+5times dd_write_tests
 
 echo
 
@@ -133,4 +139,4 @@ clear_cache
 # run fio tests
 fio xfio.conf
 
-rm test
+cleanup
