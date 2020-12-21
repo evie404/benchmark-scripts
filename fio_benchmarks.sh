1eb39212791bf84cc94d4d6de8963a6f2be0b359
Ricky Pai <rickyp999+github@gmail.com>
2020-12-20 21:25:17 -0800

    split config into different files

--- fio_benchmarks.sh
+++ fio_benchmarks.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir -p tests
-FIO_LOOPS=${FIO_LOOPS:-1} fio xfio.conf --output-format json+> results.json
-cat results.json
+
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-read.conf --output-format json+> CrystalDiskMark-read.json
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-write.conf --output-format json+> CrystalDiskMark-write.json
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-mix.conf --output-format json+> CrystalDiskMark-mix.json
+
+echo "Done."
