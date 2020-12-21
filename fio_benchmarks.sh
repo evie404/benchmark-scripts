ee3b0b8245c24864fe3ff62c2dd20edc1354c6cf
Ricky Pai <rickyp999+github@gmail.com>
2020-12-20 21:29:42 -0800

    organize results

--- fio_benchmarks.sh
+++ fio_benchmarks.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 mkdir -p tests
+mkdir -p results
 
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-read.conf --output-format json+> CrystalDiskMark-read.json
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-write.conf --output-format json+> CrystalDiskMark-write.json
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-mix.conf --output-format json+> CrystalDiskMark-mix.json
+ts=$(date +%s)
+
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-read.conf --output-format json+> results/CrystalDiskMark-$ts-read.json
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-write.conf --output-format json+> results/CrystalDiskMark-$ts-write.json
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-mix.conf --output-format json+> results/CrystalDiskMark-$ts-mix.json
 
 echo "Done."
