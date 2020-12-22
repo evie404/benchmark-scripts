bd9747bd5a9d5dce00d11758f6d2a66c86e4fdaf
Ricky Pai <rickyp999+github@gmail.com>
2020-12-22 08:48:58 -0800

    can support more jobs

--- CrystalDiskMark-fio.sh
+++ CrystalDiskMark-fio.sh
@@ -5,8 +5,8 @@ mkdir -p results
 
 ts=$(date +%s)
 
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-read.conf --output-format=json+> results/CrystalDiskMark-$ts-read.json
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-write.conf --output-format=json+> results/CrystalDiskMark-$ts-write.json
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-mix.conf --output-format=json+> results/CrystalDiskMark-$ts-mix.json
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-read.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-read.json
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-write.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-write.json
+FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-mix.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-mix.json
 
 echo "Done."
