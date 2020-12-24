65b8856ef81deda7f15d58e5e9ed944d9ce5fc1d
Ricky Pai <rickyp999+github@gmail.com>
2020-12-24 15:07:50 -0800

    configure ioengine based on os

--- CrystalDiskMark-fio.sh
+++ CrystalDiskMark-fio.sh
@@ -5,8 +5,16 @@ mkdir -p results
 
 ts=$(date +%s)
 
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-read.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-read.json
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-write.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-write.json
-FIO_LOOPS=${FIO_LOOPS:-1} fio CrystalDiskMark-mix.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-mix.json
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  FIO_IOENGINE=libaio
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  FIO_IOENGINE=posixaio
+else
+  FIO_IOENGINE=psync
+fi
+
+FIO_LOOPS=${FIO_LOOPS:-1} FIO_IOENGINE=${FIO_IOENGINE} fio CrystalDiskMark-read.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-read.json
+FIO_LOOPS=${FIO_LOOPS:-1} FIO_IOENGINE=${FIO_IOENGINE} fio CrystalDiskMark-write.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-write.json
+FIO_LOOPS=${FIO_LOOPS:-1} FIO_IOENGINE=${FIO_IOENGINE} fio CrystalDiskMark-mix.conf --max-jobs=32 --output-format=json+> results/CrystalDiskMark-$ts-mix.json
 
 echo "Done."
